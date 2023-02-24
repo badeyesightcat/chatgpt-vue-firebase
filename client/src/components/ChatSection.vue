23584ea429a36e7995caa9fa3a07b74d51c5992e
Pom Mee <whitebigbabybird@gmail.com>
2023-02-24 17:53:52 +0900

    Make ChatSection to have children

--- client/src/components/ChatSection.vue
+++ client/src/components/ChatSection.vue
@@ -2,8 +2,11 @@
   <main
     class="flex-1 flex flex-col h-screen items-center justify-center text-white bg-[#343541] px-2">
     <h1 class="text-5xl font-bold mb-20">Chat GPT</h1>
-    <div>
-      <InfoBox v-for="(item, index) in descriptions" :key="index" />
+    <div class="flex space-x-2 text-center">
+      <InfoBox
+        v-for="(item, index) in descriptions"
+        :key="index"
+        v-bind="item" />
     </div>
   </main>
 </template>
@@ -16,10 +19,37 @@ export default {
     InfoBox,
   },
   data() {
-    return () {
-      descriptions:
+    return {
+      descriptions: [
+        {
+          icon: 'sun',
+          title: 'Examples',
+          desc: [
+            'Explain quantum computing in simple terms',
+            'Got any creative ideas for a 10 year old’s birthday?',
+            'How do I make an HTTP request in Javascript?',
+          ],
+        },
+        {
+          icon: 'bolt',
+          title: 'Capabilities',
+          desc: [
+            'Remembers what user said earlier in the conversation',
+            'Allows user to provide follow-up corrections',
+            'Trained to decline inappropriate requests',
+          ],
+        },
+        {
+          icon: 'exclamationTriangle',
+          title: 'Limitations',
+          desc: [
+            'May occasionally generate incorrect information',
+            'May occasionally produce harmful instructions or biased content',
+            'Limited knowledge of world and events after 2021',
+          ],
+        },
+      ],
     };
   },
 };
 </script>
-<style></style>
