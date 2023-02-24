cfb7ade52fa12232973b5653dba1aca20c4b138d
Beom Mee <whitebigbabybird@gmail.com>
2023-02-24 20:47:07 +0900

    Working on adding authentication

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+// import firebase from 'firebase';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyASoLz6IkNAE4MfAIjQ01h7lE1Nsi3wFxE',
+//   authDomain: 'chatgpt-clone-vue.firebaseapp.com',
+//   projectId: 'chatgpt-clone-vue',
+//   storageBucket: 'chatgpt-clone-vue.appspot.com',
+//   messagingSenderId: '966759091935',
+//   appId: '1:966759091935:web:c9c811781ca7d8a573636a',
+//   measurementId: 'G-X9M661LXSL',
+// };
+// firebase.initializeApp(firebaseConfig);
+
+createApp(App).mount('#app');
