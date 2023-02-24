6cf229033b959909925217e4a2c794951dbf7f2c
Pom Mee <whitebigbabybird@gmail.com>
2023-02-24 17:53:13 +0900

    Make infobox to take props

--- client/src/components/InfoBox.vue
+++ client/src/components/InfoBox.vue
@@ -1,36 +1,47 @@
 <template>
-  <div class="flex flex-col items-center justify-center mb-5">
-    <SunIcon class="h-8 w-8" />
-    <h2>{{ title }}</h2>
-  </div>
-  <div class="space-y-2">
-    <p class="info-text" v-for="(item, index) in desc" :key="index">
-      {{ item }}
-    </p>
+  <div>
+    <div class="flex flex-col items-center justify-center mb-5">
+      <SunIcon class="h-8 w-8" v-if="icon === 'sun'" />
+      <BoltIcon class="h-8 w-8" v-if="icon === 'bolt'" />
+      <ExclamationTriangleIcon
+        class="h-8 w-8"
+        v-if="icon === 'exclamationTriangle'" />
+      <h2>{{ title }}</h2>
+    </div>
+    <div class="space-y-2">
+      <p class="info-text" v-for="(item, index) in desc" :key="index">
+        {{ item }}
+      </p>
+    </div>
   </div>
 </template>
 <script>
-import { SunIcon } from '@heroicons/vue/24/outline';
+import {
+  BoltIcon,
+  ExclamationTriangleIcon,
+  SunIcon,
+} from '@heroicons/vue/24/outline';
 
 export default {
   name: 'InfoBox',
   components: {
     SunIcon,
+    BoltIcon,
+    ExclamationTriangleIcon,
   },
   props: {
     title: {
       type: String,
-      default: 'Sample title',
+      default: '',
     },
     desc: {
       type: Array,
-      default: () => [
-        'hello, world',
-        'give me some hints on it',
-        'nah, dont need your attention',
-      ],
+      default: () => [],
+    },
+    icon: {
+      type: String,
+      default: '',
     },
   },
 };
 </script>
-<style lang=""></style>
