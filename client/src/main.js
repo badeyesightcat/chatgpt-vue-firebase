e025662fbd98553a9eb56741068c1656bf8ce37f
Beom Mee <whitebigbabybird@gmail.com>
2023-02-25 18:36:42 +0900

    Move app config values into .env file and add router

--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,9 @@
 import { createApp } from 'vue';
-// import firebase from 'firebase';
+import router from './router';
 import './style.css';
 import App from './App.vue';
 
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
-};
-firebase.initializeApp(firebaseConfig);
+const app = createApp(App);
 
-createApp(App).mount('#app');
+app.use(router);
+app.mount('#app');
