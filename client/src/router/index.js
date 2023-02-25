fa9dbe3df981fed2c1ecb3c589864823b4ad3195
Beom Mee <whitebigbabybird@gmail.com>
2023-02-25 20:35:20 +0900

    Modify ways of referring paths

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,10 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: () => import('../views/Gate.vue') },
-    { path: '/home', component: () => import('../views/Home.vue') },
-    { path: '/sign-in', component: () => import('../views/SignIn.vue') },
-    { path: '/sign-up', component: () => import('../views/SignUp.vue') },
+    { path: '/', component: () => import('@/views/Gate.vue') },
+    { path: '/home', component: () => import('@/views/Home.vue') },
+    { path: '/sign-in', component: () => import('@/views/SignIn.vue') },
+    { path: '/sign-up', component: () => import('@/views/SignUp.vue') },
   ],
 });
 
