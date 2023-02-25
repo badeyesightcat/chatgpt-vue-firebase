f76278b81cae9b879bf67e96dd292f380f7f2844
Beom Mee <whitebigbabybird@gmail.com>
2023-02-25 18:48:34 +0900

    Generate views for different routes

---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <Sidebar />
+  <ChatSection />
+</template>
+
+<script>
+import Sidebar from '../components/Sidebar.vue';
+import ChatSection from '../components/ChatSection.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Sidebar,
+    ChatSection,
+  },
+};
+</script>
