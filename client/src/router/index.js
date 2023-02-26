c42b766c99b024a16bf49a145da8a5f84ee02f50
Beom Mee <whitebigbabybird@gmail.com>
2023-02-26 19:02:12 +0900

    Add checking auth functionality to the router before going to another route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { auth } from '@/firebase/auth';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,6 +13,23 @@ const router = createRouter({
     { path: '/sign-in', component: () => import('@/views/SignIn.vue') },
     { path: '/sign-up', component: () => import('@/views/SignUp.vue') },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  if ((to.path === '/sign-in' || to.path === 'sign-up') && auth.currentUser) {
+    next('/home');
+    return;
+  }
+
+  if (
+    to.matched.some((record) => record.meta.requiresAuth) &&
+    !auth.currentUser
+  ) {
+    next('/');
+    return;
+  }
+
+  next();
 });
 
 export default router;
