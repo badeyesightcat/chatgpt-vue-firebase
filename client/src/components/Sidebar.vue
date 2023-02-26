3501d8732f6882e2ad2936b1f52f5a7c13e784b7
Beom Mee <whitebigbabybird@gmail.com>
2023-02-26 18:42:03 +0900

    Add sign-out functionality

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -7,13 +7,20 @@
         <li><a href=""></a></li>
       </ul>
     </nav> -->
+    <button @click="signOut">Sign Out</button>
   </aside>
 </template>
 <script>
+import { mapActions } from 'vuex';
 import NewChatButton from './NewChatButton.vue';
 
 export default {
   name: 'Sidebar',
   components: { NewChatButton },
+  methods: {
+    ...mapActions({
+      signOut: 'signOutAction',
+    }),
+  },
 };
 </script>
