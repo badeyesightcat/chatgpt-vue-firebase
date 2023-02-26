cb3919fdb4ecca6ede2d9326c48b86df60797d5b
Beom Mee <whitebigbabybird@gmail.com>
2023-02-26 17:12:01 +0900

    Add meta prop to let users reach to the home view after logging in

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,7 +4,11 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: () => import('@/views/Gate.vue') },
-    { path: '/home', component: () => import('@/views/Home.vue') },
+    {
+      path: '/home',
+      component: () => import('@/views/Home.vue'),
+      meta: { requiresAuth: true },
+    },
     { path: '/sign-in', component: () => import('@/views/SignIn.vue') },
     { path: '/sign-up', component: () => import('@/views/SignUp.vue') },
   ],
