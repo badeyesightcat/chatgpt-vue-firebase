d130e8975007aacd408170f2179ee1b7a763d635
Beom Mee <whitebigbabybird@gmail.com>
2023-02-26 18:43:15 +0900

    Add actions regarding authentication

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,11 @@
 import { createStore } from 'vuex';
+import {
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '@/firebase/auth';
+import router from '@/router';
 import { SET_USER, CLEAR_USER } from '@/store/mutationTypes';
 
 const store = createStore({
@@ -16,9 +23,63 @@ const store = createStore({
     },
   },
   actions: {
-    async signIn({ commit }, payload) {},
-    async signUp({ commit }, payload) {},
-    async signOut({ commit }) {},
+    async signInAction({ commit }, payload) {
+      const { email, password } = payload;
+
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+      } catch (error) {
+        switch (error.code) {
+          case 'auth/user-not-found':
+            alert('User not found.');
+            break;
+          case 'auth/wrong-password':
+            alert('Wrong password.');
+            break;
+          default:
+            alert('Something went wrong.');
+        }
+        return;
+      }
+
+      commit(SET_USER, auth.currentUser);
+      router.push('/home');
+    },
+    async signUpAction({ commit }, payload) {
+      const { email, password } = payload;
+
+      try {
+        await createUserWithEmailAndPassword(auth, email, password);
+      } catch (error) {
+        switch (error.code) {
+          case 'auth/email-already-in-use':
+            alert('Email already in use.');
+            break;
+          case 'auth/invalid-email':
+            alert('Invalid email.');
+            break;
+          case 'auth/operation-not-allowed':
+            alert('Operation not allowed.');
+            break;
+          case 'auth/weak-password':
+            alert('Weak password.');
+            break;
+          default:
+            alert('Something went wrong.');
+        }
+        return;
+      }
+
+      commit(SET_USER, auth.currentUser);
+      router.push('/home');
+    },
+    async signOutAction({ commit }) {
+      await signOut(auth);
+
+      commit(CLEAR_USER);
+
+      router.push('/');
+    },
   },
 });
 
